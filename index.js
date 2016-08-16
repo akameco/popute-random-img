2516a6cad99b1e16d10f0870faccc6e15d63a94d
akameco <akameco.t@gmail.com>
2016-08-17 07:56:25 +0900

    Update promise chain

--- index.js
+++ index.js
@@ -4,20 +4,10 @@ const cheerio = require('cheerio');
 const popute = require('popute');
 const shuffle = require('array-shuffle');
 
-module.exports = () => {
-	return popute()
-		.then(urls => {
-			return shuffle(urls)[0];
-		}).then(url =>
-			got(url).then(res => {
-				const $ = cheerio.load(res.body);
-				return $('#extMdlSeriesMngrArticle-inner78 img').attr('src');
-			})
-		).then(url =>
-			got(url, {encoding: null}).then(res => {
-				return res.body;
-			})
-		).catch(e => {
-			console.log(e);
-		});
-};
+module.exports = () =>
+	popute()
+		.then(urls => shuffle(urls)[0])
+		.then(url => got(url))
+		.then(res => cheerio.load(res.body)('#extMdlSeriesMngrArticle-inner78 img').attr('src'))
+		.then(url => got(url, {encoding: null}))
+		.then(res => res.body);
